dc6f4683b70c9747a23720aa740cdd64875f0065
taha-rouhi <taha.rouhi.dev@gmail.com>
2026-07-08 03:39:07 +0330

    Add files via upload

---
+++ On.bat
@@ -0,0 +1,2 @@
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d 192.168.1.2:8080 /f
